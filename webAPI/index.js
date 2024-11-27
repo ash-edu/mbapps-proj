613f5223c0f1a50ec191bfa4cd041b2ff034a2f7
ash <ashleyjpedu@gmail.com>
2024-11-27 15:17:39 +1100

    add GET methods for staff list and specific staff members

--- webAPI/index.js
+++ webAPI/index.js
@@ -31,8 +31,8 @@ let staffData = [
     {
         id: 1,
         name: "John Smith",
-        phone: "02",
-        department: "",
+        phone: "02 9988 2211",
+        department: 1,
         address: {
             street: "1 Code Lane",
             city: "Javaville",
@@ -97,8 +97,59 @@ let staffData = [
 
 const departments = [
     { id: 0, name: "General"},
-    { id:1, name: "Information Communications Technology" },
+    { id: 1, name: "Information Communications Technology" },
     { id: 2, name: "Finance" },
     { id: 3, name: "Marketing" },
     { id: 4, name: "Human Resources" }
 ]
+
+// GET all staff, create endpoint at URL path /staff
+app.get('/api/staff',(req,res) => {
+
+    // Create new array to store meaningful staff data, shows staff dep names not just dep ids 
+    const staffWithDeptName = staffData.map( staff => {
+        // For each staff member, find their dep  
+        const matchingDepartment = departments.find(dept => dept.id === staff.department);
+
+        // Get dep name, use 'unknown' if it doesn't exist
+        const departmentName = matchingDepartment ? matchingDepartment.name : 'Unknown';
+
+        // Create new object with original staff info, now including dep name
+        return {
+            // id: staff.id,                    # expanded list
+            // name: staff.name,
+            // phone: staff.phone,
+            // department: staff.department,
+            // departmentName: departmentName,
+            ...staff,                           // crazy concise w spread operator
+            departmentName
+        };
+    });
+
+    //send back improved staff data
+    res.json(staffWithDeptName) 
+});
+
+// GET for a specific staff member
+app.get('/api/staff/:staffId',(req,res) => {
+    let staffId = parseInt(req.params.staffId);
+    const matchingStaff = staffData.find( s => s.id === staffId);
+
+    if(matchingStaff) {
+        // Found staff member
+        const matchingDepartment = departments.find(dept => dept.id === matchingStaff.department);
+        const departmentName = matchingDepartment ? matchingDepartment.name : 'Unknown';
+
+        res.json({
+            // id: matchingStaff.id,        # expanded list
+            // name: matchingStaff.name,    
+            // phone: matchingStaff.phone,
+            // department: matchingStaff.department
+            ...matchingStaff,
+            departmentName
+        });
+    } else {
+        // No matching staff member found
+        res.status(404).json({ message: "Staff member not found"});
+    }
+});
